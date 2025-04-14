b382816196532c9addfc7925af3c8d4d7a44d13f
makoto <94143122+Sergeywws13@users.noreply.github.com>
2025-04-14 20:32:03 +0400

    commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import TELEGRAM_BOT_TOKEN
+from handlers.start import start_router
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+async def main():
+    bot = Bot(token=TELEGRAM_BOT_TOKEN)
+    dp = Dispatcher(bot=bot)
+
+    dp.include_router(start_router)
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    
---
+++ handlers/start.py
@@ -0,0 +1,31 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, FSInputFile
+
+start_router = Router()
+
+@start_router.message(Command("start"))
+async def start_command(message: Message):
+    text = """
+🌟 Добро пожаловать в Умный чат-бот LAVSORIGINS! 🌟
+
+Я здесь, чтобы раскрыть секреты красоты и здоровья вашей кожи с помощью нашего инновационного устройства.
+
+✨ Узнайте, как повседневный уход за кожей может стать идеальным ритуалом красоты.
+
+Начните ваше путешествие к совершенству уже сегодня!
+"""
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="Описание устройства", callback_data="description")],
+            [InlineKeyboardButton(text="Эффект", callback_data="effect")],
+            [InlineKeyboardButton(text="Способ применения", callback_data="use")]
+        ]
+    )
+    
+    await message.answer_photo(
+        photo=FSInputFile("images/photo_2025-04-14_20-15-21.jpg", filename="LAVSORIGINS.jpg"),
+        caption=text,
+        reply_markup=keyboard
+    )
+
